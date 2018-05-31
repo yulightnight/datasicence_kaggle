950307c11fc7a1d61d0164e2917ee4e72f24671b
喻鸣 <378331140@qq.com>
2018-05-31 22:08:26 +0800

    Create LSTMfordigits.py

---
+++ LSTMfordigits.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd 
+import tensorflow as tf
+from tensorflow.contrib import rnn
+import matplotlib.pyplot as plt
+from sklearn.utils import shuffle
+import time
+# _______________________________to be updated__________________________________
+import os
+print(os.listdir("../input"))
+
